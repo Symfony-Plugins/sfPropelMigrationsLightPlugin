245d0f1c00972b85dfb8727802e149ef1f881ac1
exien <exien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-30 16:03:05 -0500

    Fix migrate task to support symfony 1.2
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelMigrationsLightPlugin/trunk@19757 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/sfPropelMigrateTask.class.php
+++ lib/task/sfPropelMigrateTask.class.php
@@ -3,14 +3,14 @@
 /*
  * This file is part of the sfPropelMigrationsLightPlugin package.
  * (c) 2006-2008 Martin Kreidenweis <sf@kreidenweis.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
 /**
  * A symfony 1.1 port for the pake migrate task.
- * 
+ *
  * @package     sfPropelMigrationsLightPlugin
  * @subpackage  task
  * @author      Martin Kreidenweis <sf@kreidenweis.com>
@@ -22,11 +22,11 @@ class sfPropelMigrateTask extends sfPropelBaseTask
   {
     $this->addArguments(array(
       new sfCommandArgument('application', sfCommandArgument::REQUIRED, 'The application name'),
+      new sfCommandArgument('schema-version', sfCommandArgument::OPTIONAL, 'The target schema version'),
     ));
 
     $this->addOptions(array(
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
-      new sfCommandOption('schema-version', 'v', sfCommandOption::PARAMETER_REQUIRED, 'The target schema version'),
     ));
 
     $this->aliases = array('migrate');
@@ -46,9 +46,9 @@ class sfPropelMigrateTask extends sfPropelBaseTask
 
     $migrator = new sfMigrator;
 
-    if (isset($options['schema-version']) && ctype_digit($options['schema-version']))
+    if (isset($arguments['schema-version']) && ctype_digit($arguments['schema-version']))
     {
-      $runMigrationsCount = $migrator->migrate((int) $options['schema-version']);
+      $runMigrationsCount = $migrator->migrate((int) $arguments['schema-version']);
     }
     else
     {
